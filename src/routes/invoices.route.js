2f0a967290c0c2e17f6e3209ecefc37754e19592
Mayur Ahir <mayur.myatra512@hotmail.com>
2025-06-25 19:19:54 +0530

    routes positions changed

--- src/routes/invoices.route.js
+++ src/routes/invoices.route.js
@@ -14,14 +14,17 @@ invoicesRouter.get("/:companyId/number/:number", authMiddleware, (req, res) =>
   invoicesController.getInvoiceByNumber(req, res)
 );
 
-invoicesRouter.post("/:companyId", authMiddleware, (req, res) =>
-  invoicesController.createInvoice(req, res)
-);
-
 // ⚠️ THIS MUST BE BELOW ALL THE ABOVE
 invoicesRouter.get("/:companyId/:id", authMiddleware, (req, res) =>
   invoicesController.getInvoiceById(req, res)
 );
+
+invoicesRouter.post("/:companyId", authMiddleware, (req, res) =>
+  invoicesController.createInvoice(req, res)
+);
+
+
+
 
 invoicesRouter.put("/:companyId/:id", authMiddleware, (req, res) =>
   invoicesController.updateInvoice(req, res)
