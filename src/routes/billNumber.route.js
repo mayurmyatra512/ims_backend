c200b1d85fe6be6ced055af575ee750c976d6be6
Mayur Ahir <mayur.myatra512@hotmail.com>
2025-06-25 22:15:24 +0530

    changed the route

--- src/routes/billNumber.route.js
+++ src/routes/billNumber.route.js
@@ -3,12 +3,12 @@ import { Router } from "express";
 import authMiddleware from "../middleware/auth.js";
 
 const invoicesController = new InvoicesController();
-const billRouter = Router();
+const billNumberRouter = Router();
 
 
 // Define routes for invoice operations
-billRouter.get("/:companyId", authMiddleware, (req, res) =>
+billNumberRouter.get("/:companyId", authMiddleware, (req, res) =>
   invoicesController.getGeneratedBillNo(req, res)
 );
 
-export default billRouter;
+export default billNumberRouter;
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,7 @@ import userRouter from './user.route.js';
 import dashboardRouter from './dashboard.route.js';
 import bankRouter from './bank.route.js';
 import companyRouter from './company.route.js';
-import billRouter from './billNumber.route.js';
+import billNumberRouter from './billNumber.route.js';
 
 const router = express.Router();
 
@@ -17,7 +17,7 @@ router.use('/api/user', userRouter);
 router.use('/api/dashboard', dashboardRouter);
 router.use('/api/bank', bankRouter);
 router.use('/api/company', companyRouter);
-router.use('/api/billnumber', billRouter);
+router.use('/api/billnumber', billNumberRouter);
 
 router.get('/', (req, res) => {
   res.send('Welcome to the API');
