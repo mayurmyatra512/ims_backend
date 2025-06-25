1abac896d4b4759ce8efbe42ea0e9fed400aa9c0
Mayur Ahir <mayur.myatra512@hotmail.com>
2025-06-25 19:06:17 +0530

    routes positions changed

--- src/routes/invoices.route.js
+++ src/routes/invoices.route.js
@@ -6,14 +6,28 @@ import authMiddleware from "../middleware/auth.js";
 const invoicesController = new InvoicesController();
 const invoicesRouter = Router();
 // Define routes for invoice operations
-invoicesRouter.post("/:companyId", authMiddleware, (req, res) => invoicesController.createInvoice(req, res));
-invoicesRouter.get("/:companyId/generate-billno", authMiddleware, (req, res) => invoicesController.getGeneratedBillNo(req, res));
+invoicesRouter.get("/:companyId/generate-billno", authMiddleware, (req, res) => 
+    invoicesController.getGeneratedBillNo(req, res)
+);
 // This route must be above any route with /:companyId/:id to avoid being treated as an :id param
-invoicesRouter.get("/:companyId/:id", authMiddleware, (req, res) => invoicesController.getInvoiceById(req, res));
-invoicesRouter.put("/:companyId/:id", authMiddleware, (req, res) => invoicesController.updateInvoice(req, res));
-invoicesRouter.delete("/:companyId/:id", authMiddleware, (req, res) => invoicesController.deleteInvoice(req, res));
-invoicesRouter.get("/:companyId", authMiddleware, (req, res) => invoicesController.getAllInvoices(req, res));
-invoicesRouter.get("/:companyId/number/:number", authMiddleware, (req, res) => invoicesController.getInvoiceByNumber(req, res));
+invoicesRouter.get("/:companyId/:id", authMiddleware, (req, res) => 
+    invoicesController.getInvoiceById(req, res)
+);
+invoicesRouter.post("/:companyId", authMiddleware, (req, res) => 
+    invoicesController.createInvoice(req, res)
+);
+invoicesRouter.put("/:companyId/:id", authMiddleware, (req, res) => 
+    invoicesController.updateInvoice(req, res)
+);
+invoicesRouter.delete("/:companyId/:id", authMiddleware, (req, res) => 
+    invoicesController.deleteInvoice(req, res)
+);
+invoicesRouter.get("/:companyId", authMiddleware, (req, res) => 
+    invoicesController.getAllInvoices(req, res)
+);
+invoicesRouter.get("/:companyId/number/:number", authMiddleware, (req, res) => 
+    invoicesController.getInvoiceByNumber(req, res)
+);
 
 
 export default invoicesRouter;
